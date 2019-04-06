12c9580f7ec407604b6216026cd2a1bc19f2d629
Brian Gervais <briangervais@Brians-MacBook-Pro-4.local>
2019-04-06 14:53:20 -0600

    Updating about page and various element styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
 
-  var slides = $('.slides img').hide(),
-    i = 0;
+  var slides = $('.slides img');
+  i = 0;
 
-  (function cycle() {
+  function cycle() {
+    slides.hide();
     slides.eq(i).fadeIn(800)
       .delay(5000)
       .fadeOut(400, cycle);
 
       i = ++i % slides.length; // increment i,
       //   and reset to 0 when it equals divs.length
-  })();
+  }
+
+  // Execution
+  // cycle();
 
 });
