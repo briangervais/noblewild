64d2f8519b3dfd0853ffe7a48b2dc8a3b2155491
Brian Gervais <briangervais@Brians-MBP-4.domain>
2019-05-19 17:20:34 -0600

    Slick slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
 
-  var slides = $('.home-page .slides-desktop img');
-  i = 0;
-
-  function cycle() {
-    slides.hide();
-    slides.eq(i).fadeIn(800)
-      .delay(5000)
-      .fadeOut(400, cycle);
-
-      i = ++i % slides.length; // increment i,
-      //   and reset to 0 when it equals divs.length
-  }
-
-  // Execution
-  cycle();
+  $('.slick-slider').slick({
+    autoplay: true,
+    arrows: false,
+    fade: true,
+    pauseOnHover: false,
+    speed: 2000
+  });
 
 });
