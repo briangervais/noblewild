109f256ecbe0eec2099dc8669d2b24b3452422f5
Brian Gervais <briangervais@Brians-MacBook-Pro-4.local>
2019-05-29 15:08:01 -0600

    Changes to header on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
 
-  $('.slick-slider').slick({
-    autoplay: true,
-    arrows: false,
-    fade: true,
-    pauseOnHover: false,
-    speed: 2000
+  if ($('.slick-slider').length > 0) {
+    $('.slick-slider').slick({
+      autoplay: true,
+      arrows: false,
+      fade: true,
+      pauseOnHover: false,
+      speed: 2000
+    });
+  }
+
+
+  $(document).scroll(function() {
+    if($(window).scrollTop() === 0) {
+      $('body').removeClass('not-top');
+    }
+
+    else {
+      $('body').addClass('not-top');
+    }
   });
 
 });
