fcaa0e412b015423a5f353bc70d1cffd8687dc71
Brian Gervais <briangervais@Brians-MBP-4.domain>
2019-05-29 20:28:14 -0600

    Updating some styles yo

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,44 @@
 $(document).ready(function() {
 
-  if ($('.slick-slider').length > 0) {
-    $('.slick-slider').slick({
-      autoplay: true,
-      arrows: false,
-      fade: true,
-      pauseOnHover: false,
-      speed: 2000
-    });
-  }
-
-
-  $(document).scroll(function() {
+  // FUNCTION: Check Scroll Position, add positioning classes
+  function scrollCheck() {
+    // TOP
     if($(window).scrollTop() === 0) {
       $('body').removeClass('not-top');
     }
 
+    // NOT TOP
     else {
       $('body').addClass('not-top');
     }
+  }
+
+  // FUNCTION: Slick
+  function slickSlider() {
+    if ($('.slick-slider').length > 0) {
+      $('.slick--home').slick({
+        autoplay: true,
+        arrows: false,
+        fade: true,
+        pauseOnHover: false,
+        speed: 2000
+      });
+
+      $('.slick--about').slick({
+        autoplay: true,
+        fade: true,
+        pauseOnHover: false,
+        speed: 400
+      });
+    }
+  }
+
+  // On Scroll
+  $(document).scroll(function() {
+    scrollCheck();
   });
+
+  // Execute
+  slickSlider();
 
 });
